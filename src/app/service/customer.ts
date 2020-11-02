ce028734dddae1054abeea748149dd8272bace57
idoCohen <xidocohenx@Gmail.com>
2020-11-02 20:59:29 +0200

    last changes ido-side

---
+++ src/app/service/customer.ts
@@ -0,0 +1,27 @@
+import { Coupon } from 'src/app/models/coupon';
+
+
+
+
+
+
+export class CustomerService{
+
+
+
+  purchaseCoupon(): Coupon {
+
+  }
+  getCoupon(): Coupon{
+
+  }
+  getCustomerCoupons(): Coupon[]{
+    return null;
+  }
+  getCustomerCoupons(category: Category): Coupon[]{
+    return null;
+  }
+  getCustomerCoupons(price: number): Coupon[]{
+    return null;
+  }
+}
