17d31655087f6e6ce0fdd16f794f383beac2c438
OrShemesh <orshemesh5020@gmail.com>
2020-11-05 19:58:16 +0200

    changing type to Date

--- src/app/models/coupon.ts
+++ src/app/models/coupon.ts
@@ -4,13 +4,11 @@ export class Coupon {
     public categoryName: string,
     public title: string,
     public description: string,
-    public startDate: string,
-    public endDate: string,
+    public startDate: Date,
+    public endDate: Date,
     public amount: number,
     public price: number,
     public image: string,
     public id?: number
-    ) {}
-  }
-
-  // public startDate: Date,
+  ) { }
+}
