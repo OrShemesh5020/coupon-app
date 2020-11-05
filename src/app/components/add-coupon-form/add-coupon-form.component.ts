e7cf2b26ff87551cc0fde6d8d76c2145ecf53d5f
OrShemesh <orshemesh5020@gmail.com>
2020-11-05 21:34:34 +0200

    adding 'addCouponForm'

---
+++ src/app/components/add-coupon-form/add-coupon-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-coupon-form',
+  templateUrl: './add-coupon-form.component.html',
+  styleUrls: ['./add-coupon-form.component.scss']
+})
+export class AddCouponFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
