27a886294d9b49edcc119d033360897cce66e912
idoCohen <xidocohenx@Gmail.com>
2020-11-06 18:37:11 +0200

    changed all customer's parameters
    to be optionally given

--- src/app/models/customer.ts
+++ src/app/models/customer.ts
@@ -1,9 +1,9 @@
 export class Customer {
   constructor(
-    public firstName: string,
-    public lastName: string,
-    public email: string,
-    public password: string,
+    public firstName?: string,
+    public lastName?: string,
+    public email?: string,
+    public password?: string,
     public id?: number,
   ) {}
 }
