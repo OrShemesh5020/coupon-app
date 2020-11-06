c85683b42339b3572fd7713d12c778f8c3ae93ed
idoCohen <xidocohenx@Gmail.com>
2020-11-06 13:00:45 +0200

    customerForm validators addition

--- src/app/components/add-customer-form/add-customer-form.component.ts
+++ src/app/components/add-customer-form/add-customer-form.component.ts
@@ -1,15 +1,52 @@
+import { Customer } from './../../models/customer';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-customer-form',
   templateUrl: './add-customer-form.component.html',
-  styleUrls: ['./add-customer-form.component.scss']
+  styleUrls: ['./add-customer-form.component.scss'],
 })
 export class AddCustomerFormComponent implements OnInit {
-
-  constructor() { }
+  addCustomerForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
+  customerModel: Customer;
 
   ngOnInit(): void {
+    this.addCustomerForm = this.formBuilder.group({
+      // the '' shows the default value that the parameter would have upon init.
+      firstName: [
+        this.customerModel.firstName,
+         Validators.required,
+         Validators.minLength(2),
+         Validators.maxLength(10),
+
+      ],
+
+        lastName: [
+        this.customerModel.lastName,
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(15),
+
+      ],
+
+      email: [
+        this.customerModel.email,
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(20),
+          //this is the validator that makes sure the email is ended with .com
+        Validators.pattern('.com$'),
+      ],
+
+      password: [
+        this.customerModel.password,
+         Validators.required,
+         Validators.minLength(5),
+         Validators.maxLength(20),
+      ],
+    });
   }
 
 }
