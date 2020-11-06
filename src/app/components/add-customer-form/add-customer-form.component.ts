6915cb8f968a46d4c40e2078d8fa51b790448856
idoCohen <xidocohenx@Gmail.com>
2020-11-06 14:35:03 +0200

    added valuesImplementation
    and using it onSubmit

--- src/app/components/add-customer-form/add-customer-form.component.ts
+++ src/app/components/add-customer-form/add-customer-form.component.ts
@@ -1,5 +1,6 @@
+import { CustomerService } from './../../service/customer';
 import { Customer } from './../../models/customer';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCustomerFormComponent implements OnInit {
   addCustomerForm: FormGroup;
+  customerService: CustomerService;
   constructor(private formBuilder: FormBuilder) {}
   customerModel: Customer;
 
@@ -20,7 +22,6 @@ export class AddCustomerFormComponent implements OnInit {
          Validators.required,
          Validators.minLength(2),
          Validators.maxLength(10),
-
       ],
 
         lastName: [
@@ -48,5 +49,21 @@ export class AddCustomerFormComponent implements OnInit {
       ],
     });
   }
+    onSubmit(): void{
+      this.valuesImplementation();
+      // what should i do here? because the functions expects no argument, but i want to update the customerModel specifically.
+      //this.customerService.updateCustomerDetails(this.customerModel);
 
+      // and here, do i want to navigate to admin-HOme? because the admin submits the addCustomer form.
+      //this.routed.navigate(['admin/admin-home']);
+
+    }
+
+    valuesImplementation(): void {
+      this.customerModel.firstName = this.addCustomerForm.value.firstName;
+      this.customerModel.lastName = this.addCustomerForm.value.lastName;
+      this.customerModel.email = this.addCustomerForm.value.email;
+      this.customerModel.password = this.addCustomerForm.value.password;
+
+    }
 }
