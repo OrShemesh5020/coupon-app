59e30ca4eb91cd2a0543a6afdcfd6eedd8e2dbe4
idoCohen <xidocohenx@Gmail.com>
2020-11-06 16:42:36 +0200

    fixed the validators and
    added the patternvalidator
    also added the f Short-cut
    and changed the navigation to customerHome

--- src/app/components/add-customer-form/add-customer-form.component.ts
+++ src/app/components/add-customer-form/add-customer-form.component.ts
@@ -1,6 +1,13 @@
+import { Router } from '@angular/router';
 import { CustomerService } from './../../service/customer';
 import { Customer } from './../../models/customer';
-import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  EmailValidator,
+  FormBuilder,
+  FormGroup,
+  PatternValidator,
+  Validators,
+} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,60 +17,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCustomerFormComponent implements OnInit {
   addCustomerForm: FormGroup;
-  customerService: CustomerService;
-  constructor(private formBuilder: FormBuilder) {}
   customerModel: Customer;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private customerService: CustomerService
+  ) {}
 
   ngOnInit(): void {
     this.addCustomerForm = this.formBuilder.group({
       // the '' shows the default value that the parameter would have upon init.
       firstName: [
         this.customerModel.firstName,
-         Validators.required,
-         Validators.minLength(2),
-         Validators.maxLength(10),
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(45),
+        // here i tried making the function validate that the name does not contain any numbers, but the pattern validator
+        // doesnt accept a Pattern variable to validate by???
+        // i need to add ['A-Za-z'] pattern to the TEMPLATE of this class
+        new PatternValidator(),
       ],
 
-        lastName: [
+      lastName: [
         this.customerModel.lastName,
         Validators.required,
         Validators.minLength(2),
-        Validators.maxLength(15),
-
+        Validators.maxLength(45),
       ],
 
       email: [
         this.customerModel.email,
         Validators.required,
         Validators.minLength(10),
-        Validators.maxLength(20),
-          //this is the validator that makes sure the email is ended with .com
+        Validators.maxLength(45),
+        // this is the validator that makes sure the email is ended with .com
         Validators.pattern('.com$'),
       ],
 
       password: [
         this.customerModel.password,
-         Validators.required,
-         Validators.minLength(5),
-         Validators.maxLength(20),
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(45),
       ],
     });
   }
-    onSubmit(): void{
-      this.valuesImplementation();
-      // what should i do here? because the functions expects no argument, but i want to update the customerModel specifically.
-      //this.customerService.updateCustomerDetails(this.customerModel);
+  onSubmit(): void {
+    this.valuesImplementation();
+    // what should i do here? because the functions expects no argument, but i want to update the customerModel specifically.
+    this.customerService.updateCustomerDetails(this.customerModel);
 
-      // and here, do i want to navigate to admin-HOme? because the admin submits the addCustomer form.
-      //this.routed.navigate(['admin/admin-home']);
+    // and here, navigate to customer -HOme.
+    this.router.navigate(['customer/admin-home']);
+  }
 
-    }
+  valuesImplementation(): void {
+    this.customerModel.firstName = this.addCustomerForm.value.firstName;
+    this.customerModel.lastName = this.addCustomerForm.value.lastName;
+    this.customerModel.email = this.addCustomerForm.value.email;
+    this.customerModel.password = this.addCustomerForm.value.password;
+  }
+  // this is a short-cut to the controls of the form
+  get f() {
+    return this.addCustomerForm.controls;
+  }
 
-    valuesImplementation(): void {
-      this.customerModel.firstName = this.addCustomerForm.value.firstName;
-      this.customerModel.lastName = this.addCustomerForm.value.lastName;
-      this.customerModel.email = this.addCustomerForm.value.email;
-      this.customerModel.password = this.addCustomerForm.value.password;
-
-    }
 }
