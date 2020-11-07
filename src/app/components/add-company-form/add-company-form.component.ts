f29d192c7fa852420f78c62552a6bb00b2c199ad
OrShemesh <orshemesh5020@gmail.com>
2020-11-08 01:27:45 +0200

    adding add-company-form

---
+++ src/app/components/add-company-form/add-company-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-company-form',
+  templateUrl: './add-company-form.component.html',
+  styleUrls: ['./add-company-form.component.scss']
+})
+export class AddCompanyFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
