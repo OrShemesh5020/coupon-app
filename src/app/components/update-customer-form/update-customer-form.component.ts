8d19b2d177bd6fb865d5f69698a4027c3ee83fe2
idoCohen <xidocohenx@Gmail.com>
2020-11-07 15:36:17 +0200

    added valuesImplementation
    and validation check to ngOnInit

--- src/app/components/update-customer-form/update-customer-form.component.ts
+++ src/app/components/update-customer-form/update-customer-form.component.ts
@@ -1,15 +1,35 @@
+import { Customer } from 'src/app/models/customer';
+import { FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-update-customer-form',
   templateUrl: './update-customer-form.component.html',
-  styleUrls: ['./update-customer-form.component.scss']
+  styleUrls: ['./update-customer-form.component.scss'],
 })
 export class UpdateCustomerFormComponent implements OnInit {
+  updateCustomerForm: FormGroup;
+  customerModel: Customer;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSubmit() {
+    if (this.updateCustomerForm.invalid) {
+      return;
+    }
+    this.valuesImplementation();
   }
-
+  valuesImplementation() {
+    this.customerModel.firstName = this.f.firstName.value;
+    this.customerModel.lastName = this.f.lastName.value;
+    this.customerModel.email = this.f.email.value;
+    this.customerModel.password = this.f.password.value;
+  }
+  // this is a short-cut to the controls of the form
+  // tslint:disable-next-line: typedef
+  get f() {
+    return this.updateCustomerForm.controls;
+  }
 }
