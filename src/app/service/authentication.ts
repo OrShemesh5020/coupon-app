9d0f004af625e9a63c11225463a47d548def2965
OrShemesh <orshemesh5020@gmail.com>
2020-11-13 18:14:00 +0200

    adding 'getUrl' function + refactoring

--- src/app/service/authentication.ts
+++ src/app/service/authentication.ts
@@ -1,10 +1,8 @@
-import { Customer } from 'src/app/models/customer';
 import { HttpClient } from '@angular/common/http';
 import { User } from './../models/user';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { Company } from '../models/company';
 
 @Injectable({
   providedIn: 'root',
@@ -12,27 +10,27 @@ import { Company } from '../models/company';
 export class AuthenticationService {
   userSubject: BehaviorSubject<User>;
 
-  // currntUser: Subject<User> = new Subject();
-  // public user: Observable<any>;
-
   constructor(private httpClient: HttpClient) {
     this.userSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('activeUser')));
-    // this.user = this.userSubject.asObservable();
   }
 
-  public get userValue() {
-    // console.log('user value: ' + this.userSubject.value);
+  public get userValue(): User {
     return this.userSubject.value;
   }
 
+  public get getUrl(): string {
+    if (!this.userValue) {
+      return '';
+    }
+    return `home/${this.userValue.clientType.toLowerCase()}`;
+  }
 
-  public login(mail: string, pass: string) {
-    // console.log('authentication clientType: ' + clientType);
+
+  public login(mail: string, pass: string): Observable<User> {
     const url = 'http://localhost:8080/general/login';
     return this.httpClient
       .get<User>(url, {
         params: {
-          // type: clientType,
           email: mail,
           password: pass,
         },
@@ -44,29 +42,10 @@ export class AuthenticationService {
         this.userSubject.next(user);
         return user;
       }));
-    // .subscribe((res) => {
-    //   // this.user.token = res.headers.get('token');
-    //   // console.log(res.headers.get('token'));
-    //   // console.log(res.body);
-    //   const user = new User(res.body.id, res.body.email, res.body.password, res.body.clientType);
-    //   user.token = res.headers.get('token');
-    //   console.log(user);
-    //   this.loginEvent.next(user);
-    //   // this.user = value;
-    //   // console.log('login function value: ' + value);
-    //   // console.log('login function user: ' + this.user);
-    //   // // this.user.token=
-    //   // this.loginEvent.next(value);
-    // });
   }
 
-  public logout() {
-    // remove user from local storage and set current user to null
-    // console.log("i'm in log out function");
-    console.log('i"m in log out function');
+  public logout(): void {
     localStorage.removeItem('activeUser');
-    localStorage.removeItem('currentUrl');
-    // localStorage.removeItem('requestedPortal');
     this.userSubject.next(null);
   }
 }
