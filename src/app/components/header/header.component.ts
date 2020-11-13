1d17d104e96c5f48f50cc454a8b54866322c958f
OrShemesh <orshemesh5020@gmail.com>
2020-11-13 22:05:00 +0200

    redirecting the home button to the appropriate home portal

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,5 @@
-import { User } from './../../models/user';
 import { AuthenticationService } from './../../service/authentication';
-import { LayoutService } from '../../service/layout';
 import { Component, OnInit } from '@angular/core';
-import { Layout, ComponentType } from 'src/app/models/layout';
 
 @Component({
   selector: 'app-header',
@@ -10,21 +7,8 @@ import { Layout, ComponentType } from 'src/app/models/layout';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  headerItems: Layout[];
-  user: User;
-  constructor(private layoutService: LayoutService, private authentication: AuthenticationService) {
-    this.authentication.userSubject.subscribe((value: User) => {
-      console.log("HeaderComponent constructor: " + value);
-      this.user = value;
-    });
-    //   if (this.authentication.userValue) {
-    //   this.user = this.authentication.userValue;
-    //   console.log(this.user);
-    // }
-    // this.authentication.logout();
-  }
 
-  ngOnInit(): void {
-    this.headerItems = this.layoutService.getByType(ComponentType.HEADER);
-  }
+  constructor(public authentication: AuthenticationService) { }
+
+  ngOnInit(): void { }
 }
