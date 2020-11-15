1ffa53cf660263f0d44bffa8078bd811e58416f9
OrShemesh <orshemesh5020@gmail.com>
2020-11-15 04:11:47 +0200

    adding validation to all home page portals

--- src/app/service/auth-guard.ts
+++ src/app/service/auth-guard.ts
@@ -1,3 +1,5 @@
+import { ClientType } from 'src/app/models/user';
+import { User } from './../models/user';
 import { AuthenticationService } from './authentication';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
@@ -7,15 +9,54 @@ import { Injectable } from '@angular/core';
 })
 export class AuthGuard implements CanActivate {
 
+  private connectionRequired: boolean;
+  private disconnectionRequired: boolean;
+  private clientType: ClientType;
   constructor(private router: Router, private authentication: AuthenticationService) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const currentUser = this.authentication.userValue;
-    if (currentUser) {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    this.clientType = route.data.clientType;
+    this.connectionRequired = route.data.connectionRequired;
+    this.disconnectionRequired = route.data.disconnectionRequired;
+
+    if (this.disconnectionRequired) {
+      if (this.hasConnection()) {
+        this.router.navigate([this.authentication.getUrl]);
+        return false;
+      }
       return true;
     }
-    this.router.navigate(['sign-in']);
+
+    if (this.connectionRequired) {
+      if (!this.hasConnection()) {
+        this.router.navigate(['sign-in']);
+        return false;
+      }
+      if (!this.theUserTypeMatches()) {
+        this.router.navigate([this.authentication.getUrl]);
+        return false;
+      }
+      return true;
+    }
+
+
+  }
+
+  private hasConnection(): boolean {
+    if (this.currentUser) {
+      return true;
+    }
     return false;
   }
 
+  theUserTypeMatches(): boolean {
+    if (this.currentUser.clientType === this.clientType) {
+      return true;
+    }
+    return false;
+  }
+
+  private get currentUser(): User {
+    return this.authentication.userValue;
+  }
 }
