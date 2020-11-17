0d237e6ee2e8308049aad176d95571d4e6cca8c8
OrShemesh <orshemesh5020@gmail.com>
2020-11-17 13:14:15 +0200

    displaying coupons

--- src/app/components/coupon-view/coupon-view.component.ts
+++ src/app/components/coupon-view/coupon-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Coupon } from './../../models/coupon';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-coupon-view',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coupon-view.component.scss']
 })
 export class CouponViewComponent implements OnInit {
-
+  @Input() couponView: Coupon;
   constructor() { }
 
   ngOnInit(): void {
