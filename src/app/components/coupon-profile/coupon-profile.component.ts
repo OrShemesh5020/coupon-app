a21ce686871d713c1c005f2cd1c3e4891c6e9358
OrShemesh <orshemesh5020@gmail.com>
2020-11-19 03:56:09 +0200

    adding function to get coupon from the correct service + refactoring

--- src/app/components/coupon-profile/coupon-profile.component.ts
+++ src/app/components/coupon-profile/coupon-profile.component.ts
@@ -1,3 +1,11 @@
+import { CustomerService } from './../../service/customer';
+import { CompanyService } from './../../service/company';
+import { GeneralService } from './../../service/general';
+import { ClientType, User } from './../../models/user';
+import { AuthenticationService } from './../../service/authentication';
+import { Observable } from 'rxjs';
+import { Coupon } from './../../models/coupon';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +14,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coupon-profile.component.scss']
 })
 export class CouponProfileComponent implements OnInit {
-
-  constructor() { }
+  coupon: Coupon;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private authentication: AuthenticationService,
+    private router: Router,
+    private generalService: GeneralService,
+    private companyService: CompanyService,
+    private customerService: CustomerService,
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.getModel(params.id).subscribe((value: Coupon) => {
+        this.coupon = value;
+        console.log(this.coupon);
+      });
+    });
   }
 
+  getModel(id: number): Observable<Coupon> {
+    if (this.user) {
+      switch (this.user.clientType) {
+        case ClientType.COMPANY:
+          return this.companyService.getCouponById(id);
+        case ClientType.CUSTOMER:
+          return this.customerService.getCoupon(id);
+      }
+    }
+    return this.generalService.getCoupon(id);
+
+  }
+
+  private get user(): User {
+    return this.authentication.userValue;
+  }
 }
