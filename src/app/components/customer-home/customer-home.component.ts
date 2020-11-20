79257da7a8cb25b3b86dc2b05dbc879fb1428595
or shemesh <63013931+OrShemesh5020@users.noreply.github.com>
2020-11-20 17:20:08 +0200

    customer view feature (#15)
    customer view feature has been finished

--- src/app/components/customer-home/customer-home.component.ts
+++ src/app/components/customer-home/customer-home.component.ts
@@ -1,3 +1,4 @@
+import { GeneralService } from './../../service/general';
 import { AuthenticationService } from 'src/app/service/authentication';
 import { Router } from '@angular/router';
 import { Coupon } from './../../models/coupon';
@@ -12,19 +13,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerHomeComponent implements OnInit {
   customer: Customer;
-  coupons: Coupon[];
-
+  customerCoupons: Coupon[];
+  //filteredCoupons: Coupon[];
+  availableCoupons: Coupon[];
+  couponsByCategory = {};
   constructor(
     private customerService: CustomerService,
+    private generalService: GeneralService,
     private router: Router,
     private authentication: AuthenticationService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getDetails();
-    this.customerService.loadCoupons().subscribe((values: Coupon[]) => {
-      this.coupons = values;
-    });
+    this.loadCoupons();
   }
 
   getDetails(): void {
@@ -57,22 +59,63 @@ export class CustomerHomeComponent implements OnInit {
     this.customerService
       .getCustomerCouponsByCategory(categoryId)
       .subscribe((values: Coupon[]) => {
-        this.coupons = values;
-        console.log(values);
+        this.customerCoupons = values;
+        this.refreshCoupons();
       });
   }
   getCustomerCouponsByPrice(price: number): void {
     this.customerService
       .getCustomerCouponsByPrice(price)
       .subscribe((values: Coupon[]) => {
-        this.coupons = values;
-        console.log(values);
+        this.customerCoupons = values;
+        this.refreshCoupons();
       });
   }
   loadCoupons(): void {
+    this.couponsByCategory = {};
     this.customerService.loadCoupons().subscribe((values: Coupon[]) => {
-      this.coupons = values;
-      console.log(values);
+      this.customerCoupons = values;
+      this.filterByCategory();
     });
+  }
+
+  openProfile(coupon: Coupon): void {
+    // this.router.navigate[
+    //   (`${this.authentication.getUrl}/coupon-details`, coupon.id)
+    // ];
+    console.log(coupon);
+  }
+  filterByCategory(): void {
+    this.customerCoupons.forEach((coupon: Coupon) => {
+      if (!this.couponsByCategory[coupon.categoryName]) {
+        this.couponsByCategory[coupon.categoryName] = [coupon];
+      } else {
+        this.couponsByCategory[coupon.categoryName].push(coupon);
+      }
+    });
+  }
+  showAvailableCoupons(): void {
+    this.generalService.loadCoupons().subscribe((values: Coupon[]) => {
+      this.availableCoupons = values;
+    });
+  }
+  refreshCoupons(): void {
+    this.couponsByCategory = {};
+    this.filterByCategory();
+  }
+  filterCoupons(filterEelement: HTMLSelectElement): void {
+    const selectedFilter =
+      filterEelement.options[filterEelement.selectedIndex].value;
+    switch (selectedFilter) {
+      case 'couponsByCategory':
+        this.getCustomerCouponsByCategory(2);
+        break;
+      case 'couponsByPrice':
+        this.getCustomerCouponsByPrice(1000);
+        break;
+      default:
+        this.loadCoupons();
+        break;
+    }
   }
 }
