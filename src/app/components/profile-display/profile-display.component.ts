5bfea0f1b992c241ce64f1ab77bf4238ac083b5f
OrShemesh <orshemesh5020@gmail.com>
2020-11-21 04:28:29 +0200

    adding objects to keep all current user details

--- src/app/components/profile-display/profile-display.component.ts
+++ src/app/components/profile-display/profile-display.component.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs';
+import { Coupon } from './../../models/coupon';
 import { Router } from '@angular/router';
 import { ClientType, User } from './../../models/user';
 import { AuthenticationService } from './../../service/authentication';
@@ -15,7 +17,8 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileDisplayComponent implements OnInit {
   company: Company;
   customer: Customer;
-
+  status = {};
+  statistics = {};
   constructor(
     private authentication: AuthenticationService,
     private companyService: CompanyService,
@@ -27,16 +30,54 @@ export class ProfileDisplayComponent implements OnInit {
     if (this.user.clientType === ClientType.COMPANY) {
       this.companyService.getDetails().subscribe((value: Company) => {
         this.company = value;
+        this.setDetails();
+        this.setStatistics();
       });
     } else {
       this.customerService.getCustomerDetails().subscribe((value: Customer) => {
         this.customer = value;
+        this.setDetails();
+        this.setStatistics();
       });
     }
   }
 
   updateDetails(): void {
     this.router.navigate([`${this.authentication.getUrl}/update/details`]);
+  }
+
+  setStatistics(): void {
+    this.getNumOfCoupons();
+
+  }
+
+  getNumOfCoupons(): void {
+    this.getCoupons().subscribe((values: Coupon[]) => {
+      this.statistics['number of coupons'] = values.length;
+    });
+  }
+
+  getCoupons(): Observable<Coupon[]> {
+    if (this.user.clientType === ClientType.COMPANY) {
+      return this.companyService.loadCoupons();
+    }
+    return this.customerService.loadCoupons();
+  }
+
+  getDetailsKeys(object) {
+    return Object.keys(object);
+  }
+
+  setDetails(): void {
+    if (this.company) {
+      this.status['name'] = this.company.name;
+      this.status['email'] = this.company.email;
+      this.status['password'] = this.company.password;
+    } else {
+      this.status['name'] = this.customer.firstName + this.customer.lastName;
+      this.status['email'] = this.customer.email;
+      this.status['password'] = this.customer.password;
+    }
   }
 
   private get user(): User {
