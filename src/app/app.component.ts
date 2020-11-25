1b9dba921ad80ddcf56c1323f5473906b79b5c2b
OrShemesh <orshemesh5020@gmail.com>
2020-11-25 22:17:01 +0200

    changing the wrapper class of app component according to current url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { RouteConfigLoadStart, Router, NavigationStart } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 
 @Component({
@@ -7,6 +8,25 @@ import { ViewEncapsulation } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'couponSystem';
+  className = 'wrapper';
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        console.log(event.url);
+        if (event.url === '/sign-in' || event.url === '/home/public/sign-up/customer' || event.url === '/home/public/sign-up/company') {
+          this.className = 'auth-form-wrapper';
+        } else {
+          this.className = 'wrapper';
+        }
+      }
+    });
+  }
+  ngOnInit(): void {
+
+  }
+
+
+
 }
