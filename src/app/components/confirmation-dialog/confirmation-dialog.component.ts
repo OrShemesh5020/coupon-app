b914010a94f71453cbecbc2845c93c9e63158ddf
OrShemesh <orshemesh5020@gmail.com>
2020-11-30 22:22:45 +0200

    finishing editing a 'confirmation-dialog' component

--- src/app/components/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-confirmation-dialog',
@@ -8,18 +9,21 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ConfirmationDialogComponent implements OnInit {
   @Input() title: string;
   @Input() message: string;
-  @Input() btnOkText = 'ok';
-  @Input() btnCancelText = 'cancel';
-  constructor() { }
+  @Input() btnOkText: string;
+  @Input() btnCancelText: string;
+  constructor(private activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
   }
 
+  dismiss(): void {
+    this.activeModal.dismiss();
+  }
 
   decline(): void {
-
+    this.activeModal.close(false);
   }
   accept(): void {
-
+    this.activeModal.close(true);
   }
 }
