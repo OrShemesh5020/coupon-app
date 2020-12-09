8e48b194b43d7443ecdcf6e41b044ea3a1410772
Eliran Shemesh <eliran.shemesh@netomedia.com>
2020-12-09 07:24:47 +0200

    my profile css

--- src/app/components/profile-display/profile-display.component.ts
+++ src/app/components/profile-display/profile-display.component.ts
@@ -18,8 +18,31 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileDisplayComponent implements OnInit {
   company: Company;
   customer: Customer;
-  status = {};
-  statistics = {};
+  // status = {};
+  status = {
+    name: 'foo lord',
+    email: 'g983jg9j@OIJ.cc',
+    password: '1q2w3eAb'
+  };
+  // statistics = {};
+  statistics = {
+    ['Number of coupons kinds']: {
+      type: 'number',
+      value: 66666
+    },
+    ['Total coupons price']: {
+      type: 'price',
+      value: 66666
+    },
+    ['Total coupons sold']: {
+      type: 'number',
+      value: 66666
+    },
+    ['Your total sales']: {
+      type: 'price',
+      value: 66666
+    }
+  };
   constructor(
     private authentication: AuthenticationService,
     private companyService: CompanyService,
@@ -124,7 +147,8 @@ export class ProfileDisplayComponent implements OnInit {
   }
 
   getDetailsKeys(object) {
-    return Object.keys(object);
+    return ['name', 'email', 'password'];
+    // return Object.keys(object);
   }
 
   setStatus(): void {
