c4cab7924fa62127d2b1db863890f2c133b8c542
OrShemesh <orshemesh5020@gmail.com>
2020-12-12 00:45:27 +0200

    adding website-statistics component

---
+++ src/app/components/website-statistics/website-statistics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-website-statistics',
+  templateUrl: './website-statistics.component.html',
+  styleUrls: ['./website-statistics.component.scss']
+})
+export class WebsiteStatisticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
